0a0ad3bd6e8f938c1805fad2dc847f2ee0a2aa86
Denis <36292357+AUXLander@users.noreply.github.com>
2019-08-29 13:24:43 -0400

    upd

--- wp-json/test.js
+++ wp-json/test.js
@@ -34,3 +34,45 @@ var calculateHash = (secret, method, url, body) => {
 };
 
 t('707b000bc175cf58cd6aefb002a9d959')
+
+/*
+
+    testPost: function (params, callback) {
+
+        var options = {
+            url: data.baseURL + '/v1/crm/contacts',
+            jar: false,
+            json: {
+                firstName: 'Sam',
+                lastName: 'McGunner',
+                jobTitle: 'Coordinator',
+                contact_type: 'user',
+                contact_role: 'staff'
+            },
+            headers: {
+                'X-XSRF-TOKEN': '',
+                'API_KEY': params.hmacKey
+            }
+        };
+
+        var payload = calculateHash(params.hmacSecret, 'POST', '/v1/crm/contacts', JSON.stringify(options.json));
+
+        options.headers.API_SIGNATURE = payload
+
+        it('should add a contact, using hmac api', function (done) {
+            request.post(options, function (error, response, body) {
+                expect(error).to.not.exist;
+                expect(response.statusCode).to.equal(200);
+                done(callback(params));
+            });
+        });
+    }
+
+    var calculateHash = (secret, method, url, body) => {
+        var timestring = (new Date()).toISOString().split('.')[0];
+        var timestamp = timestring.substring(0, timestring.length - 3) + "Z";
+        var payload = timestamp + method + url + body;
+        var computedSignature = crypto.createHmac("sha256", secret).update(payload).digest("hex");
+        return computedSignature;
+    };
+    */
--- wp-json/test.php
+++ wp-json/test.php
@@ -15,29 +15,30 @@ function POST_Data($params){
     //$params->hmacKey = API KEY = 707b000bc175cf58cd6aefb002a9d959
     //API SECRET 9e02d1e0245166c3940f46888fd87807
 
-    $options = (object)[
+    $options = array(
         'url'   =>  $baseURL . '/v1/crm/contacts',
         'jar'   => false,
-        'json'  => (object) [
+        'json'  => array(
             'firstName'     => 'firstName',
             'lastName'      => 'lastName',
             'jobTitle'      => 'jobTitle',
             'contact_type'  => 'contact_type',
             'contact_role'  => 'contact_role'
-        ],
-        'headers' => (object) [
-            'X-XSRF-TOKEN'  => '',
-            'API_KEY'       => $params->hmacKey,
-            'API_SIGNATURE' => ''
-        ]
-    ];
+        ),
+        'headers' => array(
+            'X-XSRF-TOKEN'  => 'zoBBfiGY-lIdWxqrypSV2eU0rCt5ZOj404Zo',
+            'API_KEY'       => $params['hmacKey'],
+            'API_SIGNATURE' => '',
+            'referralMD'    => "s:TFptIpopZq9ROIS0FaVkUu4W_yzG-1hI.kVYHhthMyGilOqR3bi2lJbVI7ruBZu9/20WNHcX3TiI"
+        )
+    );
 
-    $payload = calculateHash($params->hmacKey, 'POST', '/v1/crm/contacts', json_encode($options->json));
+    $payload = calculateHash($params['hmacSecret'], 'POST', '/v1/crm/contacts', json_encode($options['json']));
 
-    $options->headers->API_SIGNATURE = $payload;
+    $options['headers']['API_SIGNATURE'] = $payload;
 
     var_dump($options);
-
+    /*
     $url = 'https://dev.getreferralmd.com/v1/profile/me/hmac-key';
     $data = array(
         'http' => array(
@@ -52,11 +53,60 @@ function POST_Data($params){
         echo '<p style="color:red">Error</p><br>';
     }
     var_dump($result);
+    */
+
+    /*
+    file_get_contents(
+        'https://dev.getreferralmd.com/v1/profile/me/hmac-key'
+        . '?'
+    );
+    */
+
+    //file_get_contents('https://dev.getreferralmd.com/login')
+
+    /*
+    $ch = curl_init('https://dev.getreferralmd.com/login');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, 
+        array(
+            'username' => 'nick@jeffpayne.net',
+            'password' => 'D4u2!@xCngzf'
+        )
+    );
+    $result = curl_exec($ch); 
+
+
+
+    preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
+    $cookies = array();
+    foreach($matches[1] as $item) {
+        parse_str($item, $cookie);
+        $cookies = array_merge($cookies, $cookie);
+    }
+    var_dump($cookies);
+    */
+
+    
+    $ch = curl_init('https://dev.getreferralmd.com/v1/profile/me/hmac-key');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $options);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    var_dump($response);
+    
 }
 
 
 echo '<pre>';
-    POST_Data((object)['hmacKey' => '9e02d1e0245166c3940f46888fd87807']);
+
+    $params = array(
+        'hmacKey'       => '707b000bc175cf58cd6aefb002a9d959',
+        'hmacSecret'    => '9e02d1e0245166c3940f46888fd87807'
+    );
+
+    POST_Data($params);
+
 echo '</pre>';
 
 
